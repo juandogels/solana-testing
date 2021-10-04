6b0cadaac094bda5bea3de580e9dfa4c84dbed2d
juandogels <suwandresukijat@gmail.com>
2021-10-04 22:26:25 +0200

    completed state.rs

--- src/processor.rs
+++ src/processor.rs
@@ -1,14 +1,15 @@
 use solana_program::{
-    account_info: {next_account_info, AccountInfo}
+    account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
-    programError::ProgramError,
+    program_error::ProgramError,
     msg,
     pubkey::Pubkey,
     program_pack::{Pack, IsInitialized},
     sysvar::{rent::Rent, Sysvar},
+    program::invoke
 };
 
-use crate::{instruction::EscrowInstruction, error::EscrowError};
+use crate::{instruction::EscrowInstruction, error::EscrowError, state::Escrow};
 
 pub struct Processor;
 impl Processor {
@@ -28,7 +29,7 @@ impl Processor {
     }
 
     fn process_init_escrow(
-        accounts: [&AccountInfo],
+        accounts: &[AccountInfo],
         amount: u64,
         program_id: &Pubkey,
     ) -> ProgramResult {
@@ -41,7 +42,7 @@ impl Processor {
 
         let temp_token_account = next_account_info(account_info_iter)?;
 
-        let token_to_receive_amount = next_account_info(account_info_iter)?;
+        let token_to_receive_account = next_account_info(account_info_iter)?;
         if *token_to_receive_account.owner != spl_token::id() {
             return Err(ProgramError::IncorrectProgramId);
         }
@@ -57,6 +58,36 @@ impl Processor {
         if escrow_info.is_initialized() {
             return Err(ProgramError::AccountAlreadyInitialized);
         }
+
+        escrow_info.is_initialized = true;
+        escrow_info.initializer_pubkey = *initializer.key;
+        escrow_info.temp_token_account_pubkey = *temp_token_account.key;
+        escrow_info.initializer_token_to_receive_account_pubkey = *token_to_receive_account.key;
+        escrow_info.expected_amount = amount;
+
+        Escrow::pack(escrow_info, &mut escrow_account.data.borrow_mut())?;
+
+        let (pda, _bump_seed) = Pubkey::find_program_address(&[b"escrow"], program_id);
+
+        let token_program = next_account_info(account_info_iter)?;
+        let owner_change_ix = spl_token::instruction::set_authority(
+            token_program.key,
+            temp_token_account.key,
+            Some(&pda),
+            spl_token::instruction::AuthorityType::AccountOwner,
+            initializer.key,
+            &[&initializer.key],
+        )?;
+
+        msg!("Calling the token program to transfer token account ownership");
+        invoke(
+            &owner_change_ix,
+            &[
+                temp_token_account.clone(),
+                initializer.clone(),
+                token_program.clone()
+            ],
+        )?;
 
         Ok(())
     }
